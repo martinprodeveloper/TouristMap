f4eeaa18454f69ed0062654872798c28b57ccece
martinprodeveloper <martinprodeveloper@gmail.com>
2024-08-01 22:53:28 -0500

    Add splash screen

---
+++ app/src/main/java/com/martinprodeveloper/touristmap/SplashActivity.kt
@@ -0,0 +1,61 @@
+package com.martinprodeveloper.touristmap
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.martinprodeveloper.touristmap.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.apply {
+            setUpViews()
+        }
+
+    }
+
+    private fun setUpViews() {
+        val text = getString(R.string.app_environment)
+        val environmentName = BuildConfig.FLAVOR
+        val animation = AnimationUtils.loadAnimation(this, R.anim.logo_animation)
+
+        binding.tvEnvironment.text = "$text $environmentName"
+        binding.ivSplash.startAnimation(animation)
+    }
+
+    private fun startSplashScreenHandler() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            goToHome()
+        }, 2500.toLong())
+    }
+
+    private fun goToHome() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startSplashScreenHandler()
+    }
+}
